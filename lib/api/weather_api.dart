bd75dfc5ec8289b4b0827e464004b423cef9a360
princeabdul90 <abuabdu99@gmail.com>
2024-09-17 06:08:31 +0800

    updated weather api script

--- lib/api/weather_api.dart
+++ lib/api/weather_api.dart
@@ -1,18 +1,15 @@
 /*
 * Developer: Abubakar Abdullahi
 * Date: 2/2/2024
-* Company: ESAT PILIPINAS TEKNIK, OPC
 */
 
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:weather/api/models/models.dart';
-import 'package:weather/api/url_builder_geocoding.dart';
-
 import 'package:weather/api/url_builder.dart';
 
 class WeatherApi {
-  static const _apiKey = 'open-weather-api-key';
+  static const _apiKey = 'open_weather_app_token';
 
   WeatherApi({
     @visibleForTesting Dio? dio,
@@ -30,20 +27,23 @@ class WeatherApi {
     const appId = String.fromEnvironment(_apiKey);
     final url = _geoCodeUrlBuilder.buildCityCoordinate(city: city, apiKey: appId);
 
+    // todo: check device location city if city input is null
+
     try{
-      final response = await _dio.get(url);
+      final response = await _dio.get(url, );
       final jsonObject = response.data;
-      final coordinate = GeocodingRM.fromJson(jsonObject['0']);
+      final coordinate = GeocodingRM.fromJson(jsonObject);
       return coordinate;
     } on DioException catch (error){
       if(error.response != null){
-        throw WeatherErrorException();
+        throw WeatherErrorRemoteException();
       }
       rethrow;
     }
   }
 
-  Future<WeatherRM?> getCityWeatherInfo(GeocodingRM geocodingRM) async {
+  Future<WeatherForecastRM?> getCityWeatherInfo(GeocodingRM geocodingRM) async {
+
     const appId = String.fromEnvironment(_apiKey);
     final url = _urlBuilder.buildSearchWeatherForecastByCoordinates(
         lat: geocodingRM.lat,
@@ -54,11 +54,16 @@ class WeatherApi {
     try {
       final response = await _dio.get(url);
       final jsonObject = response.data;
-      final weather = WeatherRM.fromJson(jsonObject);
+      final weather = WeatherForecastRM.fromJson(jsonObject);
       return weather;
     } on DioException catch (error) {
       if(error.response != null){
-        throw WeatherErrorException();
+        if(error.response?.data['cod'] == 401){
+          throw EmptyApiIdException();
+        }
+        if(error.response?.data['cod'] == 400){
+          throw WeatherErrorRemoteException();
+        }
       }
       rethrow;
     }
