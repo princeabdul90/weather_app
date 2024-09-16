76eb5215c72c6e10d37e4e3bfd9b93835056e375
princeabdul90 <abuabdu99@gmail.com>
2024-09-17 06:08:44 +0800

    updated weather url builder script

--- lib/api/url_builder.dart
+++ lib/api/url_builder.dart
@@ -1,12 +1,7 @@
 /*
 * Developer: Abubakar Abdullahi
 * Date: 2/2/2024
-* Company: ESAT PILIPINAS TEKNIK, OPC
 */
-
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-const count  = 1;
-const units  = 'metric';
 
 class UrlBuilder {
 
@@ -17,11 +12,19 @@ class UrlBuilder {
   final String _baseUrl;
 
   String buildSearchWeatherForecastByCoordinates({double? lat, double? long, String? apiKey}){
-    return '$_baseUrl?lat=$lat&lon=$long&cnt=$count&units=$units&appid=$apiKey';
+    return '$_baseUrl?lat=$lat&lon=$long&appid=$apiKey';
   }
+}
 
-  // String buildSearchWeatherForecastByCityName({String? city, String? apiKey}){
-  //   return '$_baseUrl?q=$city&cnt=$count&units=$units&appid=$apiKey';
-  // }
+class GeoCodingUrlBuilder {
 
+  final String _baseUrl;
+
+  const GeoCodingUrlBuilder({
+    String? baseUrl,
+  }) : _baseUrl = baseUrl ?? 'https://api.openweathermap.org/geo/1.0/direct';
+
+  String buildCityCoordinate({String? city, String? apiKey}) {
+    return '$_baseUrl?q=$city&limit=1&appid=$apiKey';
+  }
 }
