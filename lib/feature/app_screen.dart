d6a67a7b5e5b4f466f0c81ac279d9efc96c6f733
princeabdul90 <abuabdu99@gmail.com>
2024-09-20 10:42:23 +0800

    updated landing page ui

--- lib/feature/app_screen.dart
+++ lib/feature/app_screen.dart
@@ -8,9 +8,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
 import 'package:weather/domain_model/domain_model.dart';
+import 'package:weather/feature/widgets/loading_overlay.dart';
 import 'package:weather/repository/weather_repository.dart';
 
 import 'bloc/weather_forecast_bloc.dart';
+import 'feature.dart';
 
 class AppScreen extends StatelessWidget {
   const AppScreen({super.key});
@@ -20,12 +22,30 @@ class AppScreen extends StatelessWidget {
     return BlocProvider(
       create: (context) => WeatherForecastBloc(
         weatherRepository: context.read<WeatherRepository>(),
-      )..add(const SearchCityForecastEvent(city: "cebu")),
+      )..add( const SearchCityForecastEvent(city: '')),
       child: Scaffold(
-        body: Center(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF391A49),
+                  Color(0xFF301D5C),
+                  Color(0xFF262171),
+                  Color(0xFF301D5C),
+                  Color(0xFF391A49),
+                ],
+              ),
+          ),
+          child: WeatherCard(),
+        )
+
+        /*
+        Center(
           child: ListView(
             scrollDirection: Axis.horizontal,
-            children: const [
+            children: [
               WeatherCard(
                 imageString: 'asset/burj-khalifah.jpg',
               ),
@@ -38,18 +58,20 @@ class AppScreen extends StatelessWidget {
             ],
           ),
         ),
+        */
       ),
     );
   }
 }
 
 class WeatherCard extends StatelessWidget {
-  const WeatherCard({
+   WeatherCard({
     super.key,
-    required this.imageString,
   });
 
-  final String imageString;
+
+
+  final LoadingOverlay _loadingOverlay = LoadingOverlay();
 
   @override
   Widget build(BuildContext context) {
@@ -57,68 +79,74 @@ class WeatherCard extends StatelessWidget {
 
     return BlocConsumer<WeatherForecastBloc, WeatherForecastState>(
       listener: (context, state) {
-
         if(state.status == Status.loading){
-          const CircularProgressIndicator(color: Colors.white,);
+          _loadingOverlay.show(context);
+        }else{
+          _loadingOverlay.hide();
         }
       },
       builder: (context, state) {
-        return Container(
-          constraints: BoxConstraints.expand(
-            width: size.width,
-          ),
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(imageString), fit: BoxFit.cover)),
-          child: Stack(
-            children: [
-              Container(
-                decoration:
-                    BoxDecoration(color: Colors.black.withOpacity(0.45)),
-              ),
-              if(state.status == Status.success)
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  buildLocation(),
+        return Stack(
+          children: [
+            // Container(
+            //   decoration:
+            //       BoxDecoration(color: Colors.black.withOpacity(0.45)),
+            // ),
+            if(state.status == Status.success)
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                buildLocation(context, cityName: state.cityName),
 
-                  buildDate(day1: state.day1),
+                buildDate(day1: state.day1),
 
-                  buildCurrentWeatherTemp(data: state.day1),
+                buildCurrentWeatherTemp(data: state.day1),
 
-                  buildOtherWeatherConditions(data: state.day1),
+                buildOtherWeatherConditions(data: state.day1),
 
-                  buildOtherDaysWeather(day2: state.day2, day3: state.day3, day4: state.day4, day5: state.day5)
-                ],
-              )
-            ],
-          ),
+                buildOtherDaysWeather(day2: state.day2, day3: state.day3, day4: state.day4, day5: state.day5)
+              ],
+            )
+          ],
         );
       },
     );
   }
 
-  Widget buildLocation() {
+  Widget buildLocation(BuildContext context, {String? cityName}) {
     return Container(
-      padding: const EdgeInsets.all(10.0),
-      child: const Row(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 30),
+      child:  Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
+           Row(
             children: [
-              Icon(
+              const Icon(
                 Icons.location_pin,
                 color: Colors.white,
               ),
-              Text(
-                'Dubai',
-                style: TextStyle(color: Colors.white),
+              Text('${cityName}',
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'Roboto'),
               )
             ],
           ),
-          Icon(
-            Icons.menu,
-            color: Colors.white,
+
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const SavedLocationsScreen(),
+                ),
+              );
+            },
+            child: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
           ),
         ],
       ),
@@ -132,11 +160,18 @@ class WeatherCard extends StatelessWidget {
         Text(
           DateFormat('EEEE d').format(day1!.dtTxt!),
           style: const TextStyle(
-              color: Colors.white, fontSize: 30, fontWeight: FontWeight.w400),
+              color: Colors.white,
+              fontSize: 40,
+              fontWeight: FontWeight.w500,
+              fontFamily: 'Roboto'),
         ),
         Text(
           'Updated ${DateFormat.yMd().add_jm().format(day1.dtTxt!)}',
-          style: const TextStyle(color: Colors.white, fontSize: 10),
+          style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.w300,
+              fontFamily: 'Roboto'),
         ),
       ],
     );
@@ -154,12 +189,20 @@ class WeatherCard extends StatelessWidget {
         Text(
           '${data.weather!.first.description}',
           style: const TextStyle(
-              color: Colors.white, fontSize: 30, fontWeight: FontWeight.w400),
+              color: Colors.white,
+              fontSize: 40,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Roboto'),
         ),
-         Text(
-          '${data.main!.temp} ℃',
+
+        Text(
+          '${data.main!.temp!.toStringAsFixed(0)} ℃',
+          textAlign: TextAlign.center,
           style: const TextStyle(
-              color: Colors.white, fontSize: 48, fontWeight: FontWeight.w700),
+              color: Colors.white,
+              fontSize: 86,
+              fontWeight: FontWeight.w500,
+              fontFamily: 'Roboto'),
         ),
       ],
     );
@@ -176,7 +219,7 @@ class WeatherCard extends StatelessWidget {
         buildWeatherCondition(
             icon: Icons.thermostat_outlined,
             title: 'FEELS LIKE',
-            reading: '${data != null ? data.main!.feelsLike : '0'}°'),
+            reading: '${data != null ? data.main!.feelsLike!.toStringAsFixed(0) : '0'}°'),
       ],
     );
   }
@@ -196,25 +239,25 @@ class WeatherCard extends StatelessWidget {
             buildDayWeatherInfo(
                 date: DateFormat('EEE, d').format(day2!.dtTxt!),
                 icon: day2.weather!.first.icon,
-                temp: '${day2.main!.temp}°',
+                temp: '${day2.main!.temp!.toStringAsFixed(0)}°',
                 wind: '${day2.wind!.speed}',
             ),
             buildDayWeatherInfo(
               date: DateFormat('EEE, d').format(day3!.dtTxt!),
                 icon: day3.weather!.first.icon,
-              temp: '${day3.main!.temp}°',
+              temp: '${day3.main!.temp!.toStringAsFixed(0)}°',
               wind: '${day3.wind!.speed}',
             ),
             buildDayWeatherInfo(
               date: DateFormat('EEE, d').format(day4!.dtTxt!),
                 icon: day4.weather!.first.icon,
-              temp: '${day4.main!.temp}°',
+              temp: '${day4.main!.temp!.toStringAsFixed(0)}°',
               wind: '${day4.wind!.speed}',
             ),
             buildDayWeatherInfo(
               date: DateFormat('EEE, d').format(day5!.dtTxt!),
                 icon: day5.weather!.first.icon,
-              temp: '${day5.main!.temp}°',
+              temp: '${day5.main!.temp!.toStringAsFixed(0)}°',
               wind: '${day5.wind!.speed}',
             )
           ],
